be55b12d8b018783cd23e78c7d212515f306af4e
Vafantule <pavelgav@gmail.com>
2025-09-02 14:52:35 +0500

    add 'development project functionality'

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts_view(request):
+    success = False
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'Вы получили новое сообщение от {name}({email}): {message}')
+        success = True
+    return render(request, 'catalog/contacts.html', {'success': success})
