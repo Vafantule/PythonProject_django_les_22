8c1d5c600f703f01b01edb0fb5c5bbafa3f6202a
Vafantule <pavelgav@gmail.com>
2025-09-11 18:21:04 +0500

    add controller & template about product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from .views import product_detail_view
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('contacts/', views.contacts_view, name='contacts'),
+    path("product/<int:pk>", views.product_detail_view, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home_view(request):
@@ -14,3 +15,11 @@ def contacts_view(request):
         print(f'Вы получили новое сообщение от {name}({email}): {message}')
         success = True
     return render(request, 'catalog/contacts.html', {'success': success})
+
+
+def product_detail_view(request, pk: int):
+    """
+    Контроллер для отображения страницы с подробной информацией о товаре.
+    """
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "catalog/product_detail.html", {"product": product})
