402430a929125ecb7668d35e08c9f288a7292fe8
Vafantule <pavelgav@gmail.com>
2025-09-19 15:56:52 +0500

    add controller's & template's for app project

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,66 @@
-from django.urls import path
-from . import views
+from django import forms
+from .models import BlogPost
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-urlpatterns = [
-    path('', views.BlogListView.as_view(), name='blog_list'),
-]
+
+class BlogPostForm(forms.ModelForm):
+    """
+    Форма создания и редактирования записей блога.
+    """
+    class Meta:
+        model = BlogPost
+        fields = ["title", "content", "preview", "is_published"]
+
+
+class BlogListView(ListView):
+    """
+    Контроллер отображения списка записей блогов.
+    """
+    model = BlogPost
+    template_name = "blog/blog_list.html"
+    context_object_name = "blogs"
+
+
+class BlogDetailView(DetailView):
+    """
+    Контроллер отображения одной записи блога.
+    """
+    model = BlogPost
+    template_name = "blog/blog_detail.html"
+    context_object_name = "blog"
+    pk_url_kwarg = "pk"
+
+
+class BlogCreateView(CreateView):
+    """
+    Контроллер создания записи блога.
+    """
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = "blog/blog_form.html"
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogUpdateView(UpdateView):
+    """
+    Контроллер обновления записи блога.
+    """
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = "blog/blog_form.html"
+    success_url = reverse_lazy("blog:blog_list")
+    pk_url_kwarg = "pk"
+    context_object_name = "blog"
+
+
+class BlogDeleteView(DeleteView):
+    """
+    Контроллер удаления записи блога.
+    """
+    model = BlogPost
+    template_name = "blog/blog_delete_confirm.html"
+    success_url = reverse_lazy("blog:blog_list")
+    pk_url_kwarg = "pk"
+    context_object_name = "blog"
