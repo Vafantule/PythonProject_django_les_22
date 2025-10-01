84e821bb1e64fd683d72d5a9b081d48f4d7334de
Vafantule <pavelgav@gmail.com>
2025-10-01 12:48:52 +0500

    add app_name in url.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserListView, UserDetailView, UserCreateView, UserUpdateView, UserDeleteView
 
+app_name = 'users'
+
 
 urlpatterns = [
     path("", UserListView.as_view(), name="user_list"),
