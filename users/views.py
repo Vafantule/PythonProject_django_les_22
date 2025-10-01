7b42eaec7d46744db68406a5400b50b4c5466491
Vafantule <pavelgav@gmail.com>
2025-10-01 13:44:59 +0500

    add controller for register users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from pyexpat.errors import messages
+
+from .forms import UserRegistrationForm
 from .models import CustomUser
+from typing import Any
 
 
 class UserListView(ListView):
@@ -49,3 +54,26 @@ class UserDeleteView(DeleteView):
     model = CustomUser
     template_name = 'users/user_confirm_delete.html'
     success_url = reverse_lazy('user_list')
+
+
+class UserRegistrationView(FormView):
+    """
+    Контроллер регистрации пользователя.
+    """
+    template_name = "users/registration.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("user:user_login")
+
+    def form_valid(self, form: UserRegistrationForm) -> Any:
+        """
+        Создание пользователя и отправление сообщений.
+        """
+        user = form.save()
+        send_mail(
+            subject="Добро пожаловать на сайт.",
+            message="Спасибо за регистрацию на сайте!",
+            from_email=None,
+            recipient_list=[user.email],
+            fail_silently=False
+        )
+        return super().form_valid(form)
