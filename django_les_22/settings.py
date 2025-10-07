e88147f38bbfc1919c5c9a08a412e35edc8f9e78
Vafantule <pavelgav@gmail.com>
2025-10-07 12:12:59 +0500

    init setting's for redis module

--- django_les_22/settings.py
+++ django_les_22/settings.py
@@ -132,3 +132,13 @@ EMAIL_USE_SSL  = os.getenv("EMAIL_USE_SSL", "False") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+# Cashes
+CACHE_ENABLED = True
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
